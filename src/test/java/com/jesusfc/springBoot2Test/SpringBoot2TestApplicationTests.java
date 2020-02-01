b5e499ce66b12e89f80844227fd84c37aa2be88d
Jesúsfc <jfcaraballo@gmail.com>
2020-02-01 21:57:49 +0100

    first commit

---
+++ src/test/java/com/jesusfc/springBoot2Test/SpringBoot2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesusfc.springBoot2Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
