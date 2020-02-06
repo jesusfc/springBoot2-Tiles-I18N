2b8930707a006fef04f02e73ea576935190e5bd1
Jesús Fdez. Caraballo <jfcaraballo@gmail.com>
2020-02-06 10:37:26 +0100

    Update to MVC + Thymeleaf

--- src/main/java/com/config/SpringBoot2TestApplication.java
+++ src/main/java/com/config/SpringBoot2TestApplication.java
@@ -1,9 +1,9 @@
-package com.jesusfc.springBoot2Test;
+package com.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com"})
 public class SpringBoot2TestApplication {
 
 	public static void main(String[] args) {
