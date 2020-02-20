8beace2064a30e7556f7a1422b311714541fa132
Jesús Fdez. Caraballo <jesus.fernandez@jumbotours.com>
2020-02-20 12:25:02 +0100

    update files for configuration and run CI

--- src/test/java/com/jesusfc/springBoot2Test/SpringBoot2TestApplicationTests.java
+++ src/test/java/com/jesusfc/springBoot2Test/SpringBoot2TestApplicationTests.java
@@ -1,9 +1,8 @@
 package com.jesusfc.springBoot2Test;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SpringBoot2TestApplicationTests {
 
 	@Test
